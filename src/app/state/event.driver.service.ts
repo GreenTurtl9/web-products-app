f9f634b465623fb1372428813695257ea9f7721a
bsaoudi <bilel.seoudi@gmail.com>
2022-04-22 01:05:35 +0200

    event driver service

---
+++ src/app/state/event.driver.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from "@angular/core";
+import { Subject } from "rxjs";
+import { ActionEvent } from "./product.state";
+
+@Injectable({ providedIn: "root" })
+export class EventDriverService {
+
+    sourceEventSubject: Subject<ActionEvent> = new Subject<ActionEvent>();
+}
